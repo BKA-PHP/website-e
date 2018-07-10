3caf076dd8eea6116bf46ee20d743e9fa9ef3d8a
Trung <thangman240175@gmail.com>
2018-07-10 21:21:08 +0700

    add controller
    áaaa

---
+++ controller.php
@@ -0,0 +1,14 @@
+ci <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Controller extends CI_Controller {
+
+	public function index()
+	{
+		
+	}
+
+}
+
+/* End of file controller.php */
+/* Location: .//C/Users/DELL/Documents/GitHub/website-e/controller.php */
